f381d8d5bbe22708621346fa6195fd3bd3543026
DavitD1996 <davit.sargsyan@picsart.com>
2024-01-21 21:52:37 +0400

    The Homework for the 4th lesson

--- CustomConversion/Vector.cs
+++ CustomConversion/Vector.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework1
+namespace Homework1.CustomConversion
 {
-    internal interface Itest<T>
+    internal class Vector
     {
-        T test();
     }
 }
---
+++ Indexer/Matrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1.Indexer
+{
+   public class Matrix
+    {
+        private int[,] data;
+        public Matrix(int columns, int rows)
+        {
+            data = new int[rows,columns];
+        }
+        public int Columns { get { return data.GetLength(1); } }
+        public int Rows { get { return data.GetLength(0); } }
+        public int this[int row, int column]
+        {
+            get {
+                if (IsDataValid(row, column)){
+                    return data[row, column];
+                }
+                Console.WriteLine("incorrect data");
+                return 0;
+            }
+            set { if (IsDataValid(row, column))
+                {
+                    data[row, column] = value;
+                }
+                else {
+                    Console.WriteLine("incorrect data");
+                    return;
+                } 
+            }
+        }
+        private bool IsDataValid(int row, int column)
+        {
+            return (row < Rows && column < Columns);
+        }
+
+    }
+}
